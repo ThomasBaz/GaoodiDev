3bf9b85d7f4c80826a6f3f8fcd7c2d43cf95ccf3
Thomas BAZINET <tbazinet@KLEE.LAN.NET>
2018-09-28 14:06:48 +0200

    Fin de la partie UI de la mailBox

--- src/MailBox.js
+++ src/MailBox.js
@@ -23,6 +23,31 @@ class ContactLine extends React.Component {
 	}
 }
 
+class MessageLine extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+		}
+	}
+
+	render() {
+
+		let classes = "messageLine";
+		if(this.props.from == "me") {
+			classes += " sendedMsg";
+		} else {
+			classes += " recievedMsg";
+		}
+
+		return(
+			<div className={classes}>
+				{this.props.msgContent}
+			</div>
+		)
+	}
+}
+
 class MailBox extends React.Component {
 	constructor(props) {
 		super(props);
@@ -52,7 +77,12 @@ class MailBox extends React.Component {
 					</div>
 					<div className="MailBoxMessagesContent">
 						<div className="messagesContainer ">
-						
+							<MessageLine from={"me"} msgContent={"Hello ! comment ça va ?"}/>
+							<MessageLine from={"him"} msgContent={"Hey ! Super et toi ?"}/>
+							<MessageLine from={"me"} msgContent={"Tip top !"}/>
+							<MessageLine from={"him"} msgContent={"Génial !"}/>
+							<MessageLine from={"me"} msgContent={"Je fais juste un petit test avec un message un peu plus conséquent, histoire de voir si le css suit bien et que ça n'a pas trop une salle geule (en gros)."}/>
+							<MessageLine from={"him"} msgContent={"Je vois ça. C'est pas trop mal ;) "}/>
 						</div>
 						<div className="sendMessageContainer ">
 							<textarea placeholder={"Ecrivez un message ..."}  className="mailBoxMessageInput"></textarea>
@@ -67,7 +97,23 @@ class MailBox extends React.Component {
 						Dictoinnaire
 					</div>
 					<div className="MailBoxTradContent">
-						
+						<div className="langSelection">
+							{"Depuis :"}
+							<select id="fromLocale">
+								<option value="eng">Anglais</option> 
+								<option value="fr" selected>Français</option>
+								<option value="es">Espagnol</option>
+							</select>
+							{"Vers :"}
+							<select id="toLocale">
+								<option value="eng" selected>Anglais</option> 
+								<option value="fr">Français</option>
+								<option value="es">Espagnol</option>
+							</select>
+						</div>
+						<textarea placeholder={"Texte à traduire"}  className="mailBoxMessageInput"></textarea>
+						<div className="separatorLine"/>
+						<textarea readOnly className="mailBoxMessageInput">{"Text to traduce"}</textarea>
 					</div>
 				</div>
 			</div>
